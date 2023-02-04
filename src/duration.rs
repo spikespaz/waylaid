34399e2fb0970c8556fe26da91fa967b7338e6b2
Jacob Birkett <jacob@birkett.dev>
2023-02-04 01:24:08 -0700

    duration: add proper error type

--- src/duration.rs
+++ src/duration.rs
@@ -1,11 +1,16 @@
 pub mod parser {
     use once_cell::sync::Lazy;
     use regex::{Match, Regex};
+    use thiserror::Error;
 
     static DURATION_RE_STR: &str = r#"(?i)(?:(?P<h>\d+)h)? ?(?:(?P<m>\d+)m)? ?(?:(?P<s>\d+)s)?"#;
     static DURATION_RE: Lazy<Regex> = Lazy::new(|| Regex::new(DURATION_RE_STR).unwrap());
 
-    pub fn parse_duration(input: &str) -> Result<u64, ()> {
+    #[derive(Error, Debug, PartialEq, Clone, Copy)]
+    #[error("failed to parse string as a duration")]
+    pub struct ParseDurationError;
+
+    pub fn parse_duration(input: &str) -> Result<u64, ParseDurationError> {
         let caps = DURATION_RE.captures(input.trim()).unwrap();
         let to_s = |x| move |m: Match| m.as_str().parse::<u64>().unwrap() * x;
         let h = caps.name("h").map(to_s(60 * 60));
@@ -14,7 +19,7 @@ pub mod parser {
         let parts = &[h, m, s];
 
         if parts.iter().all(Option::is_none) {
-            Err(())
+            Err(ParseDurationError)
         } else {
             Ok(parts.iter().fold(0, |a, b| match b {
                 Some(s) => a + s,
@@ -25,7 +30,7 @@ pub mod parser {
 
     #[cfg(test)]
     mod tests {
-        use super::parse_duration;
+        use super::{parse_duration, ParseDurationError};
         use test_case::test_case;
 
         #[test_case("2h 2m 30s", Ok(7350))]
@@ -34,10 +39,10 @@ pub mod parser {
         #[test_case("2m30s", Ok(150))]
         #[test_case("30s", Ok(30))]
         #[test_case("2m", Ok(120))]
-        #[test_case("1", Err(()))]
-        #[test_case("m", Err(()))]
-        #[test_case("", Err(()))]
-        fn test_parse_duration(input: &str, expect: Result<u64, ()>) {
+        #[test_case("1", Err(ParseDurationError))]
+        #[test_case("m", Err(ParseDurationError))]
+        #[test_case("", Err(ParseDurationError))]
+        fn test_parse_duration(input: &str, expect: Result<u64, ParseDurationError>) {
             assert_eq!(parse_duration(input), expect)
         }
     }
